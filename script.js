808b09aa3cfb61560af278bb330ebf9938330c6d
Alexandre-Paulo-Silva <alexandre.paulo.silva@hotmail.com>
2021-07-19 18:24:03 -0400

    Quarto commit

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+
+document.querySelector(".hamburguer").addEventListener("click", () =>  /*função de envento */
+document.querySelector(".container").classList.toggle("show-menu")  /* Seletor para o menu Hamburguer*/
+);
