82c5512e724cba385c4a31afe8aa2372f0dd8988
Alexandre-Paulo-Silva <alexandre.paulo.silva@hotmail.com>
2021-08-07 22:16:49 -0300

    11

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>  /*função de envento */
-document.querySelector(".container").classList.toggle("show-menu")  /* Seletor para o menu Hamburguer*/
+    document.querySelector(".container").classList.toggle("show-menu")  /* Seletor para o menu Hamburguer*/
 );
